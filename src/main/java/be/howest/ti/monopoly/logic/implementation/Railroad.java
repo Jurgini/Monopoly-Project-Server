04cb61ba14beaa898e04a44a10b23e702c1c1adb
tuur.delacroix <tuur.delacroix@student.howest.be>
2022-05-03 22:35:31 +0200

    created railroad tile

---
+++ src/main/java/be/howest/ti/monopoly/logic/implementation/Railroad.java
@@ -0,0 +1,20 @@
+package be.howest.ti.monopoly.logic.implementation;
+
+public class Railroad extends Property {
+    private final int rent;
+    private final String color;
+
+    public Railroad(String name, int position, String type) {
+        super(name, position, type, 2000, 1000);
+        this.rent = 250;
+        this.color = "BLACK";
+    }
+
+    public int getRent() {
+        return rent;
+    }
+
+    public String getColor() {
+        return color;
+    }
+}
